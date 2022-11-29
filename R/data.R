e17ca438cad2b766ab251d98b4ce936b23fb18cb
Hao Ye <lhopitalified@gmail.com>
2022-11-29 12:52:32 -0500

    add dataset add function add documentation for dataset and function

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Example data.frame
+#'
+#' @format A data frame with 2 columns:
+#' \describe{
+#'   \item{x}{some numbers}
+#'   \item{y}{some other numbers}
+#' }
+"dat"
---
+++ R/my-functions.R
@@ -0,0 +1,9 @@
+#' Get the column names of a data.frame
+#' @param df a data.frame
+#' @return a character vector
+#'
+#' @export
+f <- function(df)
+{
+    names(df)
+}
